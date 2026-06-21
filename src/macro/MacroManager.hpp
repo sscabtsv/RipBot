5e629383fee55c1455281843c28b39f25fb59294
sscabtsv <anastacioaijhenweina@gmail.com>
2026-06-21 19:35:15 +0800

    Introduce MacroEvent struct and update MacroManager
    Added functionality to track macro events with a new struct.

--- src/macro/MacroManager.hpp
+++ src/macro/MacroManager.hpp
@@ -1,4 +1,12 @@
 #pragma once
+#include <vector>
+
+struct MacroEvent {
+    int frame;
+    int button;
+    bool player2;
+    bool push;
+};
 
 class MacroManager {
 private:
@@ -10,9 +18,15 @@ private:
 public:
     static MacroManager* get();
 
+    int currentFrame = 0;
+    size_t playbackIndex = 0;
+    std::vector<MacroEvent> events;
+
     bool isRecording() const;
     void setRecording(bool state);
 
     bool isPlaying() const;
     void setPlaying(bool state);
+
+    void addEvent(int button, bool player2, bool push);
 };
